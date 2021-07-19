57f98361f7bfb4f5bc3c0f251132d513828c8427
matthias361 <87650815+matthias361@users.noreply.github.com>
2021-07-19 17:28:48 +0200

    Add files via upload

---
+++ Abgaben/Quiz_Matthias_Kilian.py
@@ -0,0 +1,41 @@
+
+
+
+
+cr7store =  109
+cr7=int(input('Wie viele Tore hat Ronaldo für Portugal geschossen? '))
+
+score = 0
+
+if cr7store == cr7:
+    score += 1
+    print('Das ist die Richtige Antwort!')
+
+else:
+    print('Das ist falsch, die richtige Antwort ist 109! '
+          '')
+
+ballon = 5
+odor=int(input('Wie oft hat Ronaldo den Ballon Odor gewonnen? ') )
+
+if ballon == odor:
+    score += 1
+    print('Das ist die Richtige Antwort!')
+
+else:
+    print('Das ist falsch, die richtige Antwort ist 5! '
+          '')
+
+verein = BARCELONA = 0
+antwort= input('Bei welchem Verein spielt Messi? ') .upper()
+
+if antwort == BARCELONA:
+        score += 1
+        print('Das ist die richtige Antwort! ')
+
+else:
+    print('Das ist falsch, die richtige Antwort ist BARCELONA! '
+          '')
+
+print(f'Ergebnis == {score}! ')
+
