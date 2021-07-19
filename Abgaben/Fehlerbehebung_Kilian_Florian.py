fe4444ff738a626e03770d808ec46af9daf05697
Kilian365 <87650813+Kilian365@users.noreply.github.com>
2021-07-19 16:18:49 +0200

    Add files via upload

---
+++ Abgaben/Fehlerbehebung_Kilian_Florian.py
@@ -0,0 +1,11 @@
+'''author: Christine Ackerl
+date: 18. 07. 2021
+Dieses Script berechnet die Fläche eines Kreises bei gegebenem Radius.
+Es enthält einige Fehler. Findest du sie?
+Schreibe die jeweiligen Fehlermeldungen als Kommentare neben die jeweilige Zeile und korrigiere den Code.
+'''
+
+radius = int(input('Gib den Radius eines Kreises in cm ein! '))
+pi = 3.1415692
+flaeche = radius ** 2 * pi
+print(f'Die Fläche des Kreises beträgt {flaeche} Quadratzentimeter')
