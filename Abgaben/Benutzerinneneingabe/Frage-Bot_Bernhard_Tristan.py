04be4dc5ae71e476c842d3cf54ce756f4d6c263a
TwoB00m <87650485+TwoB00m@users.noreply.github.com>
2021-07-19 16:34:10 +0200

    Add files via upload

--- Abgaben/Benutzerinneneingabe/Frage-Bot_Bernhard_Tristan.py
+++ Abgaben/Benutzerinneneingabe/Frage-Bot_Bernhard_Tristan.py
@@ -5,4 +5,5 @@ Username = input("Whats your Minecraft username?")
 print("Hello " + Name + " aka " + Username)
 response = requests.get("https://api.mojang.com/users/profiles/minecraft/" + Username)
 Minecraft = input("Do you want to see your Minecraft UUID?")
-print(response.json())
+if Minecraft == 'YES':
+    print(response.json())
