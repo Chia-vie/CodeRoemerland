d7a5dc1b6aaab6620feaf77bcf0825597078e803
TwoB00m <87650485+TwoB00m@users.noreply.github.com>
2021-07-19 15:27:34 +0200

    Add files via upload

---
+++ Abgaben/MadLips_Bernhard_Matthias.py
@@ -0,0 +1,4 @@
+Adjektiv1 = input("Gib bitte das 1.Adjektiv ein")
+Verb = input("Gib bitte ein Verb ein")
+Adjektiv2 = input("Gib bitte das 2.Adjektiv ein")
+print("Wir " + Verb + " " + Adjektiv1 + " ,ein " + Adjektiv2 + " Python Programm!")
