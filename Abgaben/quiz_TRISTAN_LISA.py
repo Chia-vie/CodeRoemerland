953661780e87d17fab62723ecfbbea47bf5e74ed
Tristan Kahler <87650835+tristancokage@users.noreply.github.com>
2021-07-19 17:56:15 +0200

    Add files via upload

--- Abgaben/quiz_TRISTAN_LISA.py
+++ Abgaben/quiz_TRISTAN_LISA.py
@@ -1,10 +1,13 @@
-print('Last die spiele beginnen')
-antwort=input('wozu haben Kamele höcker?')
-if antwort=='weil sie damit länger ohne waser aus kommen':
-    print('richtig!!!!!!')
-Antwort=input('wo haben Katzen ihre krallen am liebsten?')
-if Antwort=='im sofa':
-    print('Richtig!!!!')
-    AAntwort=input('was tun hunde am liebsten?')
-    if AAntwort=='jagen':
-        print('alles richtig')
+
+while True:
+    print('Last die spiele beginnen')
+    antwort = input('wozu haben Kamele höcker?')
+    if antwort == 'weil sie damit länger ohne waser aus kommen':
+        print('richtig!!!!!!')
+        Antwort = input('wo haben Katzen ihre krallen am liebsten?')
+        if Antwort == 'im sofa':
+            print('Richtig!!!!')
+            AAntwort = input('was tun hunde am liebsten?')
+            if AAntwort == 'jagen':
+                print('alles richtig')
+                break
