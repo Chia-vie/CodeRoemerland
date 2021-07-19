3d88110cd3038d5155ee9129cfc2070571188cd8
Chia-vie <christine.ackerl@univie.ac.at>
2021-07-19 15:28:25 +0200

    Geschwindigkeitskonverter_Christine.py MadLibs_Christine.py

---
+++ Abgaben/Geschwindigkeitskonverter_Christine.py
@@ -0,0 +1,11 @@
+'''
+author: Christine
+date: 19.07.2021
+Das ist ein Geschwindigkeitskonverter
+'''
+
+Geschwindigkeit = float(input('Gib eine Geschwindigkeit in km/h ein '))
+
+Meterprosekunde = Geschwindigkeit/3.6
+
+print(f'Das sind {Meterprosekunde} m/s')
---
+++ Abgaben/MadLibs_Christine.py
@@ -0,0 +1,7 @@
+Adjektiv = input('Gib ein Adjektiv ein! ').upper()
+Nomen = input('Gib ein Nomen ein! ').upper()
+Verb = input('Gib ein Verb ein! ').upper()
+
+print(f'HEUTE IST ES DRAUSSEN RICHTIG {Adjektiv}.')
+print(f'DESHALB GEHE ICH MIT MEINER {Nomen} SPAZIEREN.')
+print(f'SPÄTER WERDEN WIR NOCH {Verb}.')
